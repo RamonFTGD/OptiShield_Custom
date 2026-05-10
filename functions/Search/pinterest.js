f865d60cb2873c2637545a48a55b2ca4ac4ad0f8
Ramón <165967820+RamonFTGD@users.noreply.github.com>
2026-05-10 19:24:28 +0000

    Update pinterest.js

--- functions/Search/pinterest.js
+++ functions/Search/pinterest.js
@@ -15,10 +15,6 @@ export default async function (msg, sock, ctx) {
     await sock.sendMessage(jid, { text: '❌ Debes escribir algo.\nEjemplo: `.pinsearch gatos`' }, { quoted: msg })
     return true
   }
-  if (!apikey) {
-    await sock.sendMessage(jid, { text: '❌ No tienes API Key válida.' }, { quoted: msg })
-    return true
-  }
 
   const statusMsg = await sock.sendMessage(jid, {
     text: `📌 Buscando *"${query}"* en Pinterest...`
