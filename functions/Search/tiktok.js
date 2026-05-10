8acf5c317eadcf016f96f4e88abb79ebdc28a0de
Ramón <165967820+RamonFTGD@users.noreply.github.com>
2026-05-10 19:24:58 +0000

    Update tiktok.js

--- functions/Search/tiktok.js
+++ functions/Search/tiktok.js
@@ -27,11 +27,6 @@ export default async function (msg, sock, ctx) {
     return true
   }
 
-  if (!apikey) {
-    await sock.sendMessage(jid, { text: '❌ No tienes API Key válida.' })
-    return true
-  }
-
   const statusMsg = await sock.sendMessage(jid, {
     text: `🎵 Buscando en TikTok: *${query}*...`
   }, { quoted: msg })
