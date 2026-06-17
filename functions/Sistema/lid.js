63a815abf9e42e530a53eb617e6dc88528eccb8d
Ramón <165967820+RamonFTGD@users.noreply.github.com>
2026-06-16 21:08:14 -0600

    Update lid.js

--- functions/Sistema/lid.js
+++ functions/Sistema/lid.js
@@ -2,7 +2,7 @@ import { getUserLevel, getLevelName } from '../../lib/security.js'
 
 export const meta = {
   name: 'lid',
-  commands: ['lid', 'userinfo', 'me'],
+  commands: ['lid', 'userinfo'],
   priority: 5,
   class: 'Sistema',
   desc: 'Muestra la información del usuario que utilizó el comando',
